dd41878039ecc6853e85e970df52c2b71239e820
SNagirniy <86325813+SNagirniy@users.noreply.github.com>
2026-04-11 22:48:33 +0300

    upgrade config db

--- my-strapi-app/config/database.ts
+++ my-strapi-app/config/database.ts
@@ -23,20 +23,18 @@ export default ({ env }) => {
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+  connection: {
+    connectionString: env('DATABASE_URL'),
+    ssl: {
+      rejectUnauthorized: false,
     },
+    schema: env('DATABASE_SCHEMA', 'public'),
+  },
+  pool: {
+    min: env.int('DATABASE_POOL_MIN', 2),
+    max: env.int('DATABASE_POOL_MAX', 10),
+  },
+},
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
